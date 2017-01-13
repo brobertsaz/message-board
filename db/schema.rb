1c00bd571999467464c498505d3771b8f566bd66
Bob Roberts <bob@rebel-outpost.com>
2017-01-12 23:34:09 -0600

    forgot them there timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113042342) do
+ActiveRecord::Schema.define(version: 20170113053120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,11 @@ ActiveRecord::Schema.define(version: 20170113042342) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string  "title"
-    t.text    "body"
-    t.integer "user_id"
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
